6667d269c05ed85b770275b1f74a2eafc5aed11f
arpan7sarkar <arpansakar@gmail.com>
2025-10-30 23:31:39 +0530

    added profile fetching api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,34 @@
 import { Outlet } from "react-router";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  //this function will call profile viewing api when the website will be loaded
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data.user));
+    } catch (error) {
+      console.log(error);
+      
+    }
+  };
+  useEffect(()=>{
+    fetchUser();
+  },[ ]);
   return (
     <div>
       <NavBar />
-      <Outlet /> 
-      <Footer/>
+      <Outlet />
+      <Footer />
     </div>
   );
 };
