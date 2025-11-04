c1aae059c9e555715edd937eb6a13ec3cb76b5ef
arpan7sarkar <arpansakar@gmail.com>
2025-11-04 23:01:38 +0530

    making profile edit card dynamic

--- src/Page/Profile.jsx
+++ src/Page/Profile.jsx
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import EditProfile from "../components/EditProfile";
 
 const Profile = () => {
+  const user=useSelector((state)=>state.user);
   return (
     <div>
-        <EditProfile />
+        <EditProfile user={user}/>
     </div>
   );
 };
--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
-const EditProfile = () => {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
-  const [age, setAge] = useState(18);
+import { useEffect, useState } from "react";
+const EditProfile = ({ user }) => {
+  const { fName, lName, age, about, photoUrl,gender } = user;
+  const [Fname, setFName] = useState("");
+  const [Lname, setLName] = useState("");
+  const [Age, setAge] = useState(18);
+  const [profilePic, setProfilePic] = useState("");
+  const [Gender, setGender] = useState("");
+  const [About, setAbout] = useState("Hey there I am using Syntax social");
+  const getUserDetails =()=>{
+    setFName(fName);
+    setLName(lName);
+    setAge(age);
+    setAbout(about);
+    setProfilePic(photoUrl);
+    setGender(gender);
+  }
+  useEffect(()=>{
+    getUserDetails();
+  },[user])
   return (
-    <div className="h-screen w-full flex justify-center mt-30 r">
+    user ? <div className="h-screen w-full flex justify-center mt-30 r">
       <div className="h-3/4 w-1/3 rounded-2xl overflow-hidden ">
         {/* Profile section */}
         <div className="bg-base-200 h-full w-full  p-2">
@@ -12,8 +27,10 @@ const EditProfile = () => {
             PROFILE
           </div>
           {/* Profile Pic Link */}
-          <div >
-            <legend className="fieldset-legend text-xl p-2">Profile Pic Link</legend>
+          <div>
+            <legend className="fieldset-legend text-xl p-2">
+              Profile Pic Link
+            </legend>
             <input
               type="text"
               className="input w-full p-4"
@@ -30,15 +47,15 @@ const EditProfile = () => {
                 <input
                   type="text"
                   className="input p-4 w-1/2"
-                  placeholder="First Name"
-                  value={fName}
+                  placeholder={Fname}
+                  value={Fname}
                   onChange={(e) => setFName(e.target.value)}
                 />
                 <input
                   type="text"
                   className="input p-4 w-1/2"
-                  placeholder="Last Name"
-                  value={lName}
+                  placeholder={Lname}
+                  value={Lname}
                   onChange={(e) => setLName(e.target.value)}
                 />
               </div>
@@ -50,7 +67,7 @@ const EditProfile = () => {
               <legend className="fieldset-legend text-xl p-2">Age</legend>
               <select
                 className="select p-4 h-15"
-                value={age}
+                value={Age}
                 onChange={(e) => setAge(e.target.value)}
               >
                 {[18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30].map(
@@ -77,7 +94,10 @@ const EditProfile = () => {
             <legend className="fieldset-legend text-xl p-2">About</legend>
             <textarea
               className="textarea p-4 w-full h-40"
-              placeholder="Write something about yourself"
+              placeholder={About}
+              value={
+                About
+              }
             ></textarea>
           </fieldset>
 
