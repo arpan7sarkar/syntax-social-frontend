89dbaa2b6ef98878d49d9685127f4bd8f6af66b5
arpan7sarkar <arpansakar@gmail.com>
2025-11-10 21:57:08 +0530

    added remove request

--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -1,16 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const requestSlice=createSlice({
-    name:"request",
-    // default to empty array so components can safely map()
-    initialState: [],
-    reducers: {
-        addRequest:(state,action)=>{
-            // replace state with payload (expected array)
-            return action.payload || [];
-        },
+const requestSlice = createSlice({
+  name: "request",
+  // default to empty array so components can safely map()
+  initialState: [],
+  reducers: {
+    addRequest: (state, action) => {
+      // replace state with payload (expected array)
+      return action.payload || [];
     },
-
-})
-export const {addRequest}=requestSlice.actions;
+    removeRequest: (state, action) => {
+      const userId = action.payload;
+      const newState = state.filter((item) => item._id !== userId);
+      return newState;
+    },
+  },
+});
+export const { addRequest, removeRequest } = requestSlice.actions;
 export default requestSlice.reducer;
