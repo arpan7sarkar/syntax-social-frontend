e39f2ba9cfa556111cfc7a5e7e76db7498e05f66
arpan7sarkar <arpansakar@gmail.com>
2025-11-27 19:45:11 +0530

    created NavBar and added the bg for the landing page

--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import LandingNavBar from "../components/LandingNavBar";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="min-h-screen bg-[#050505] text-white font-sans selection:bg-white/20 selection:text-white overflow-x-hidden relative">
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        <div className="absolute top-[-20%] left-1/2 -translate-x-1/2 w-[80%] h-[60%] bg-white/[0.03] blur-[120px] rounded-full"></div>
+        <div className="absolute top-[10%] left-1/2 -translate-x-1/2 w-[120%] h-[800px] border-t border-white/10 rounded-[100%] bg-gradient-to-b from-white/[0.02] to-transparent pointer-events-none"></div>
+      </div>
 
-export default LandingPage
+      <LandingNavBar />
+    </div>
+  );
+};
+
+export default LandingPage;
